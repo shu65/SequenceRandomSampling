1700a1ce77bd62f64ae78d5e9f308cf9e405efb2
shu65 <dolphinripple@gmail.com>
2013-10-05 22:19:29 +0900

    Modifed bugs

--- sequencerandomsampling.py
+++ sequencerandomsampling.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import sys
+import random
 from optparse import OptionParser
 
 from Bio.Seq import Seq
@@ -9,152 +10,84 @@ from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna
 from Bio.Alphabet import IUPAC
+from numpy.core.fromnumeric import sort
+
 
 def get_options():
     parser = OptionParser()
-    parser.add_option("-a", "--ssearch_result", dest="ssearch_result_filename",
-                  help="SSEARCH result file name", metavar="FILE")
+    parser.add_option("-i", "--input_filename", dest="input_filename",
+                  help="input fasta filename", metavar="FILE")
     
-    parser.add_option("-c", "--comparing_result", dest="comparing_result_filename",
-                  help="Result file name to compare", metavar="FILE")
+    parser.add_option("-o", "--output_filename", dest="output_filename",
+                  help="Output fasta filename", metavar="FILE")
     
-    parser.add_option("-t", "--tool", dest="tool",
-                  help="comparing tool name : blast, ghostx", type="string")
+    parser.add_option("-p", "--percent", dest="percent",
+                  help="sampling percent (%)", type="float", default=0.0)
     
-    parser.add_option("-x", "--x_axis", dest="x_axis",
-                  help="x axis : E-value (e), Bit-score (b), Identity (i)", type="string")
+    parser.add_option("-n", "--number", dest="number",
+                  help="number of subsequences ", type="int", default=0)
     
-    parser.add_option("-s", "--x_start", dest="x_start", default= -1.0,
-                  help="x start value", type="float")
+    parser.add_option("-f", "--sequence_file_format", dest="file_format",
+                  help="sequence file format (fasta, fastq)", default="fasta")
     
-    parser.add_option("-e", "--x_end", dest="x_end", default= -1.0,
-                  help="x end value", type="float")
+    parser.add_option("-s", "--seed", dest="seed",
+                       help="random seed", type="int", default=0)
     
-    parser.add_option("-p", "--print_mishit", action="store_true", dest="print_mishit",
-                  help="print mis hit result")
-
     return parser.parse_args()
+
+
+def GetNumberOfSequences(input_handle, file_format):
+    return len([None for record in SeqIO.parse(input_handle, file_format)])
+
+
+def WriteSequences(input_handle, file_format, number_sequence, number_sampling_sequences, output_handle):
+    sampling_sequence_ids = random.sample(xrange(0, number_sequence), number_sampling_sequences)
+    sampling_sequence_ids.sort()
+
+    print sampling_sequence_ids
+
+    sequences_i = 0
+    sampling_sequence_ids_i = 0
+    for record in SeqIO.parse(input_handle, file_format):
+        if sequences_i == sampling_sequence_ids[sampling_sequence_ids_i]:
+            SeqIO.write(record, output_handle, file_format)
+            sampling_sequence_ids_i = sampling_sequence_ids_i + 1
+            if sampling_sequence_ids_i >= number_sampling_sequences:
+                return
+            
+        sequences_i = sequences_i + 1
+    
+    return
+
 
 def main():
     (options, args) = get_options()
-    ssearch_file = open(options.ssearch_result_filename)
-    ssearch_reader = SSearchResultReader(ssearch_file)
-    x_axis = ""
-    if options.x_axis == "e":
-        x_axis = "e_value"
-    elif options.x_axis == "b":
-        x_axis = "bit_score"
-    elif options.x_axis == "i":
-        x_axis = "identity"
+    if options.seed == 0:
+        random.seed(None)
     else:
-        sys.exit("errors : x axis is invalided")
-    x_start, x_end, iterating_op, iterating_value = get_x_axis_default(ssearch_reader, x_axis)
-    ssearch_file.close()
+        random.seed(options.seed)
+    number_sampling_sequences = 0
+    input_handle = open(options.input_filename)
+    number_sequence = GetNumberOfSequences(input_handle, options.file_format)
+    input_handle.close()
+    if options.percent == 0.0 and options.number == 0:
+        sys.exit("error: unset -p and -n.")
+    elif options.percent > 0:
+        number_sampling_sequences = int(number_sequence*options.percent/100)
+    else:
+        number_sampling_sequences = options.number
     
-    if options.x_start != -1:
-        x_start = options.x_start
-    if options.x_end != -1:
-        x_end = options.x_end
+    if number_sampling_sequences > number_sequence:
+        sys.exit("error: number sampling sequences is over.")
+    input_handle = open(options.input_filename)
+    output_handle = open(options.output_filename, "w")
     
-    if x_start > x_end:
-        sys.exit("errors : x start and end")
-        
-    x_range = get_x_range(x_axis, iterating_op, iterating_value, x_start, x_end)
+    WriteSequences(input_handle, options.file_format, number_sequence, number_sampling_sequences, output_handle)
     
-    ssearch_file = open(options.ssearch_result_filename)
-    ssearch_reader = SSearchResultReader(ssearch_file)
-    comparing_file = open(options.comparing_result_filename)
-    comparing_reader = BlastResultReader(comparing_file);
-    # comparing_reader = ExtendingBlastResultReader(comparing_file);
-    hit_list, sum_list, number_ignored_alignments = compare_ssearch(ssearch_reader, comparing_reader, x_axis, x_range, options.print_mishit)
-    output(x_range, hit_list, sum_list, number_ignored_alignments)
-    
-    ssearch_file.close()
-    comparing_file.close()
-    
-
-usage = "usage: %prog [options]"
-parser = OptionParser(usage)
-
-parser.add_option(
-    "-i", "--input", 
-    action="store",
-    type="string",
-    dest="input",
-    help="input fasta file"
-)
+    input_handle.close()
+    output_handle.close()
 
 
-parser.add_option(
-    "-o", "--output",
-    action="store",
-    type="string", 
-    dest="output",
-    help="output fasta file prifix",
-)
-
-
-parser.add_option(
-    "-n", 
-    action="store",
-    type="int",
-    dest="number",
-    help="division number"
-)
-
-(options, args) = parser.parse_args()
-
-input_file  = options.input
-output_file = options.output + "_"
-
-number_records = 0
-input_handle = open(input_file, "r")
-for record in SeqIO.parse(input_handle, "fasta"):
-    number_records = number_records + 1
-
-stride = int((number_records + options.number - 1)/options.number)
-
-input_handle.close()
-input_handle = open(input_file, "r")
-for i in range(options.number):
-    records = []
-    chunk_size = 0
-    for record in SeqIO.parse(input_handle, "fasta"):
-        records.append(record)
-        chunk_size = chunk_size + 1
-        if chunk_size >= stride :
-            break
-
-    out_handle = open(output_file + str(i), "w")
-    for record in records[:] :
-        SeqIO.write(record, out_handle, "fasta")
-
-    out_handle.close()
-
-input_handle.close()
-'''
-records = []
-for record in SeqIO.parse(input_file, "fasta"):
-    records.append(record)
-
-number_records = len(records)
-stride = int((number_records + options.number - 1)/options.number)
-
-for i in range(options.number):
-    out_handle = open(output_file + str(i), "w")
-    for record in records[stride*i:stride*(i + 1)] :
-        SeqIO.write(record, out_handle, "fasta")
-
-    out_handle.close()
-
-
-Created on 2013/10/07
-
-@author: shu
-'''
-
-        
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-        
         
