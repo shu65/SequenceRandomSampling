041bbeac2bb79b8666f73550e31538b13e64434b
Shuji Suzuki <suzuki@bi.cs.titech.ac.jp>
2013-10-08 13:39:40 +0900

    add option to input paired files

--- sequencerandomsampling.py
+++ sequencerandomsampling.py
@@ -15,8 +15,8 @@ from numpy.core.fromnumeric import sort
 
 def get_options():
     parser = OptionParser()
-    parser.add_option("-i", "--input_filename", dest="input_filename",
-                  help="input fasta filename", metavar="FILE")
+    parser.add_option("-i", "--input_filenames", dest="input_filenames",
+                  help="input fasta filename (if paired files, use \",\" to separate)", metavar="FILE")
     
     parser.add_option("-o", "--output_filename", dest="output_filename",
                   help="Output fasta filename", metavar="FILE")
@@ -40,22 +40,23 @@ def GetNumberOfSequences(input_handle, file_format):
     return len([None for record in SeqIO.parse(input_handle, file_format)])
 
 
-def WriteSequences(input_handle, file_format, number_sequence, number_sampling_sequences, output_handle):
+def WriteSequences(input_handles, file_format, number_sequence, number_sampling_sequences, output_handles):
     sampling_sequence_ids = random.sample(xrange(0, number_sequence), number_sampling_sequences)
     sampling_sequence_ids.sort()
 
     print sampling_sequence_ids
 
-    sequences_i = 0
-    sampling_sequence_ids_i = 0
-    for record in SeqIO.parse(input_handle, file_format):
-        if sequences_i == sampling_sequence_ids[sampling_sequence_ids_i]:
-            SeqIO.write(record, output_handle, file_format)
-            sampling_sequence_ids_i = sampling_sequence_ids_i + 1
-            if sampling_sequence_ids_i >= number_sampling_sequences:
-                return
+    for input_handles_i in range(0, len(input_handles)):
+        sequences_i = 0
+        sampling_sequence_ids_i = 0
+        for record in SeqIO.parse(input_handles[input_handles_i], file_format):
+            if sequences_i == sampling_sequence_ids[sampling_sequence_ids_i]:
+                SeqIO.write(record, output_handles[input_handles_i], file_format)
+                sampling_sequence_ids_i = sampling_sequence_ids_i + 1
+                if sampling_sequence_ids_i >= number_sampling_sequences:
+                    break
             
-        sequences_i = sequences_i + 1
+            sequences_i = sequences_i + 1
     
     return
 
@@ -66,10 +67,12 @@ def main():
         random.seed(None)
     else:
         random.seed(options.seed)
+    input_filenames = options.input_filenames.split(",")
     number_sampling_sequences = 0
-    input_handle = open(options.input_filename)
-    number_sequence = GetNumberOfSequences(input_handle, options.file_format)
-    input_handle.close()
+    input_handles = []
+    input_handles.append(open(input_filenames[0]))
+    number_sequence = GetNumberOfSequences(input_handles[0], options.file_format)
+    input_handles[0].close()
     if options.percent == 0.0 and options.number == 0:
         sys.exit("error: unset -p and -n.")
     elif options.percent > 0:
@@ -79,13 +82,21 @@ def main():
     
     if number_sampling_sequences > number_sequence:
         sys.exit("error: number sampling sequences is over.")
-    input_handle = open(options.input_filename)
-    output_handle = open(options.output_filename, "w")
+    input_handles[0] = open(input_filenames[0])
+    output_handles = []
+    if len(input_filenames) == 2:
+        input_handles.append(open(input_filenames[1]))
+        output_handles.append(open(options.output_filename + ".1", "w"))
+        output_handles.append(open(options.output_filename + ".2", "w"))
+    else:
+        output_handles.append(open(options.output_filename, "w"))
     
-    WriteSequences(input_handle, options.file_format, number_sequence, number_sampling_sequences, output_handle)
     
-    input_handle.close()
-    output_handle.close()
+    WriteSequences(input_handles, options.file_format, number_sequence, number_sampling_sequences, output_handles)
+    
+    for i in range(0, len(input_handles)):
+        input_handles[i].close()
+        output_handles[i].close()
 
 
 if __name__ == '__main__':
